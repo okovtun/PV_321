3f739d1d0afaf7d168fb1d526f11bd1dd3fbd5e0
Clayman <Clayman@DESKTOP-DM8E77U>
2023-10-09 21:46:17 +0300

    Tank DONE TODO: Check fill()

---
+++ Introduction/Car/main.cpp
@@ -0,0 +1,61 @@
+﻿#include<iostream>
+using namespace std;
+
+#define MIN_TANK_VOLUME	 35
+#define MAX_TANK_VOLUME	160
+class Tank
+{
+	const int VOLUME;	//Характеристика объекта
+	double fuel_level;	//Состояние объекта
+public:
+	int get_VOLUME()const
+	{
+		return VOLUME;
+	}
+	double get_fuel_level()const
+	{
+		return fuel_level;
+	}
+	void fill(double fuel)
+	{
+		if (fuel < 0)return;
+		if (fuel + this->fuel_level > VOLUME)this->fuel_level = VOLUME;
+		else this->fuel_level += fuel;
+	}
+	double give_fuel(double amount)
+	{
+		fuel_level -= amount;
+		if (fuel_level < 0)fuel_level = 0;
+		return fuel_level;
+	}
+	Tank(int volume) :VOLUME(
+			volume < MIN_TANK_VOLUME ? MIN_TANK_VOLUME : 
+			volume > MAX_TANK_VOLUME ? MAX_TANK_VOLUME : 
+			volume), 
+		fuel_level(0)
+	{
+		/*
+		if (volume < 35)volume = 35;
+		if (volume > 160)volume = 160;
+		this->VOLUME = volume;
+		*/
+		cout << "TConstructor:\t" << this << endl;
+	}
+	~Tank()
+	{
+		cout << "TDestructor:\t" << this << endl;
+	}
+
+	void info()const
+	{
+		cout << "Volume:\t" << get_VOLUME() << " liters.\n";
+		cout << "Fuel:\t" << get_fuel_level() << " liters.\n";
+	}
+};
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	Tank tank(20);
+	tank.info();
+}
---
+++ Introduction/Introduction/main.cpp
@@ -0,0 +1,28 @@
+﻿#include<iostream>
+using namespace std;
+
+struct Point
+{
+	double x;
+	double y;
+};
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	//type name;
+	int a;		//Объявление переменной 'a' типа 'int'
+	Point A;	//Объявление переменной 'A' типа 'Point'
+				//Создание объекта 'A' структуры 'Point'
+				//Создание экземпляра 'A' структуры 'Point'
+	A.x = 2;
+	A.y = 3;
+	cout << A.x << "\t" << A.y << endl;
+}
+
+/*
+---------------------------------
+.
+->
+---------------------------------
+*/
